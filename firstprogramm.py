bcfa83e28522ec3f723d3640843f7ced02b638ba
Ram Prasad <ramprasad.udutha0369@gmail.com>
2025-08-04 10:21:15 +0530

    Add files via upload

---
+++ firstprogramm.py
@@ -0,0 +1,8 @@
+# print(2+5)
+# a = int(input('enter a number'))
+# b = int(input('enter a number'))
+# sum = a+b
+# print(sum)
+a = int(input('enter a value: '))
+b = int(input('enter a value: '))
+print(a>=b)
